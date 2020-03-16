31f76683611c48961e93fa7622e1d666e697be27
Kellertw <keller.tw@gmail.com>
2020-03-16 18:48:15 -0400

    update

--- QuickDBD-export (1).sql
+++ QuickDBD-export (1).sql
@@ -1,28 +1,24 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-DROP TABLE IF EXISTS "Asset_Fofeiture";
+DROP TABLE IF EXISTS "Asset_Forfeiture";
 
-CREATE TABLE "Asset_Fofeiture" (
-    "id" SERIAL,
+CREATE TABLE "Asset_Forfeiture" (
     "City" VARCHAR(500)   NOT NULL,
     "State_abr" VARCHAR(500)   NOT NULL,
     "State" VARCHAR(500)   NOT NULL,
     "Description" VARCHAR(500)   NOT NULL,
-    "Equity" int   NOT NULL,
-    "Fiscal_Year" date   NOT NULL,
-     PRIMARY KEY ("id")
+    "Equity" VARCHAR(500)   NOT NULL,
+    "Fiscal_Year" VARCHAR(500)   NOT NULL
 );
 
 DROP TABLE IF EXISTS "SAR_Statistics";
 CREATE TABLE "SAR_Statistics" (
-    "id" SERIAL,
-    "Year" VARCHAR(4)    NOT NULL,
+    "Year" VARCHAR(500)    NOT NULL,
     "State" VARCHAR(500)   NOT NULL,
     "Industry" VARCHAR(500)   NOT NULL,
     "Suspicious_Activity" VARCHAR(500)   NOT NULL,
-    "Count" int   NOT NULL,
-     PRIMARY KEY ("id")
+    "Count" int NOT NULL
 );
 
 
